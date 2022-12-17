28f84de18dd13796c1bd2f70f434589cd054a973
ToastyandTasty <mahvogel@gmail.com>
2022-12-17 10:27:56 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const addition = (num1,num2) => {
+    return num1 + num2;
+}
+const multiplication =  (num1) => {
+    return num1 * 2;
+}
+const addResult = addition(4,14);
+// console.log(addResult);
+const multiOutput = multiplication(addResult);
+console.log(multiOutput);
